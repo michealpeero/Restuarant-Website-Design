b07e57e03f1707bdb801b2c3876e23863a16fa2a
Rameez Hassan <168276824+michealpeero@users.noreply.github.com>
2025-05-29 17:55:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const closeIcone=document.querySelector('.close-icon')
+const navigation=document.querySelector('nav')
+
+closeIcone.addEventListener('click', (e)=>{
+    
+        navigation.style.display="none";
+    
+})
